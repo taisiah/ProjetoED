49e80fd4fc472126c56c646c10b5be6dcb21c0b0
taisiah <taisiahonorio@gmail.com>
2020-10-20 15:09:09 -0300

    Update biblioteca.py
    inicio

---
+++ biblioteca.py
@@ -0,0 +1,27 @@
+
+
+
+
+def cadastro_livro():
+    livro=[]
+    titulo=input("Digite o titulo do livro")
+    autor=input("Digite o autor do livro:")
+    ano=int(input("Digite o ano do livro:"))
+    editora=input("Digite a editora do livro:")
+    edicao=input("Digite a localização do livro")
+
+    print("Livro cadastrado com sucesso!")
+
+
+def remove_livro():
+
+    print("Título removido com sucesso!")
+
+def status():
+
+def busca():
+
+def relatorio():
+
+def main():
+
