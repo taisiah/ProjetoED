d31c0a88f32187963f023d802b31116c5bdd56a2
taisiah <taisiahonorio@gmail.com>
2020-10-23 09:53:13 -0300

    Update biblioteca.py

--- biblioteca.py
+++ biblioteca.py
@@ -1,14 +1,23 @@
 
 
+livros = {}
+
+nome = "java"
+autor = "pedro"
+ano = 2010
+
+livros[nome] = [autor,ano]
 
 
 def cadastro_livro():
+    código =
     livro=[]
     titulo=input("Digite o titulo do livro")
     autor=input("Digite o autor do livro:")
     ano=int(input("Digite o ano do livro:"))
     editora=input("Digite a editora do livro:")
     edicao=input("Digite a localização do livro")
+    quantidade= int(input("Digite uma quantidade"))
 
     print("Livro cadastrado com sucesso!")
 
@@ -19,9 +28,18 @@ def remove_livro():
 
 def status():
 
-def busca():
+def buscar():
+    busca = input("Digite nome para busca")
+    if 'busca' ==
 
 def relatorio():
 
 def main():
+    livro = []
+    titulo = input("Digite o titulo do livro")
+    autor = input("Digite o autor do livro:")
+    ano = int(input("Digite o ano do livro:"))
+    editora = input("Digite a editora do livro:")
+    edicao = input("Digite a localização do livro")
+    quantidade = int(input("Digite uma quantidade"))
 
