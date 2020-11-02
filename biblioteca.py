17d95410ca1abe2989a5abd9db01d644d50add8d
João Henrique Garcia <joaogarcia.sistemas@gmail.com>
2020-11-02 12:22:21 -0300

    Update biblioteca.py

--- biblioteca.py
+++ biblioteca.py
@@ -21,6 +21,27 @@ def login (login,senha):
             return f[0]
     return None
 
+def cadastro_categoria ():
+    print("ADICIONAR CATEGORIA")
+    categoria = input("Digite o nome da categoria que deseja cadastrar: ")
+    if categoria in categorias :
+        print("Categoria existente")
+    else:
+        categorias.append(categoria)
+        print("Categoria adicionada com sucesso!")
+        categorias.sort()
+
+
+def cadastro_tematica ():
+    print("ADICIONAR TEMÁTICA")
+    tematica = input("Digite o nome da temática que deseja cadastrar: ")
+    if tematica in tematicas :
+        print("Temática existente")
+    else:
+        tematicas.append(tematica)
+        print("Temática adicionada com sucesso!")
+        tematicas.sort()
+
 
 def cadastro_livro():
     print("\nCADASTRO DE LIVRO")
@@ -36,20 +57,7 @@ def cadastro_livro():
     lista_livros.append(livro)
 
     print("Livro cadastrado com sucesso!")
-
-
-def remove_livro(tituloLivro):
-    global lista_livros
-    print("\nREMOVER LIVRO")
-
-    for i in range (0, len(lista_livros)):
-        if (lista_livros[i]['titulo']) == tituloLivro:
-            print (lista_livros[i])
-            lista_livros.pop(i)
-            break
-
-    print(f"Livro {tituloLivro} removido com sucesso!")
-
+    
 
 def ajusta_acervo(tituloLivro):
     global lista_livros
@@ -67,11 +75,18 @@ def ajusta_acervo(tituloLivro):
     print(f'A nova quantidade de acervo do livro {lista_livros[i]["titulo"]} é {lista_livros[i]["quantidade"]} un') 
 
 
-'''
-def status():
-    if status == True :
-def relatorio():
-'''
+def remove_livro(tituloLivro):
+    global lista_livros
+    print("\nREMOVER LIVRO")
+
+    for i in range (0, len(lista_livros)):
+        if (lista_livros[i]['titulo']) == tituloLivro:
+            print (lista_livros[i])
+            lista_livros.pop(i)
+            break
+
+    print(f"Livro {tituloLivro} removido com sucesso!")
+
 
 def busca_livro(tituloLivro):
     global lista_livros
@@ -85,11 +100,18 @@ def busca_livro(tituloLivro):
             print (f"Editora..: {lista_livros[i]['editora']}")
             break
 
-def repeteFuncao(funcao):
+def repete_funcao(funcao):
     continua = 'S'
     while continua in 'Ss':
         funcao()
         continua = input('Fazer novo cadastro [S/N]? ')
+
+
+'''
+def status():
+    if status == True :
+def relatorio():
+'''
 
 
 def menu():
@@ -110,29 +132,10 @@ def menu():
     return input("\nDigite a opção desejada: ")
 
 
-def cadastro_categoria ():
-    print("ADICIONAR CATEGORIA")
-    categoria = input("Digite o nome da categoria que deseja cadastrar: ")
-    if categoria in categorias :
-        print("Categoria existente")
-    else:
-        categorias.append(categoria)
-        print("Categoria adicionada com sucesso!")
-        categorias.sort()
-
-def cadastro_tematica ():
-    print("ADICIONAR TEMÁTICA")
-    tematica = input("Digite o nome da temática que deseja cadastrar: ")
-    if tematica in tematicas :
-        print("Temática existente")
-    else:
-        tematicas.append(tematica)
-        print("Temática adicionada com sucesso!")
-        tematicas.sort()
-
 def main():
     print("\n=-=-=-=-=-= MORAIS LIBRARY - Sistema de Gestão Bibliotecária =-=-=-=-=-=")
     tentativas = 0
+    
     while True:
         login_func = input("\nDigite seu login: ")
         senha_func = input("Digite sua senha: ")
@@ -150,11 +153,11 @@ def main():
             while True:
                 opcao = menu()
                 if opcao == '1':
-                    repeteFuncao(lambda: cadastro_categoria())
+                    repete_funcao(lambda: cadastro_categoria())
                 elif opcao == '2':
-                    repeteFuncao(lambda: cadastro_tematica())
+                    repete_funcao(lambda: cadastro_tematica())
                 elif opcao == '3':
-                    repeteFuncao(lambda: cadastro_livro())
+                    repete_funcao(lambda: cadastro_livro())
                 elif opcao == '4':
                     #printar função enquanto não faz
                     print("HABILITAR RESERVA")
